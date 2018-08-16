068109e97426bf1dabae474ab2413f9a8c5dfe51
Jason Sinn <jrs341@me.com>
2018-08-16 01:14:23 -0500

    added keys to itterations

--- src/components/LineChart/MinorAxis.js
+++ src/components/LineChart/MinorAxis.js
@@ -30,12 +30,12 @@ export default class MinorAxis extends Component {
       if (i === 1 || i === 2) {
         i === 1
           ?  minorXAxis.push([<line key={'minorX' + i} x1='3' y1='3.12' x2={svgWidth - 5} y2='3.12' style={{stroke:'red', strokeWidth:'.2'}}/>,
-              <text x='0' y='3.6' style={{fill: 'red', fontSize:'1.5'}}> 8.96 </text>])
+              <text key={'minorXText' + i} x='0' y='3.6' style={{fill: 'red', fontSize:'1.5'}}> 8.96 </text>])
           :  minorXAxis.push([<line key={'minorX' + i} x1='3' y1={i * 3} x2={svgWidth -5 } y2={i * 3} style={{stroke:'orange', strokeWidth:'.1'}}/>,
-              <text x='1' y={(i * 3) + .4} style={textStyle}> {(i * 3) + 2} </text>])  
+              <text key={'minorXText' + i}x='1' y={(i * 3) + .4} style={textStyle}> {(i * 3) + 2} </text>])  
       } else {
           minorXAxis.push([<line key={'minorX' + i} x1='3' y1={i * 3} x2={svgWidth - 5} y2={i * 3} style={{stroke:'black', strokeWidth:'.1'}}/>,
-            <text x='1' y={(i * 3) + .4} style={textStyle}> {(30 - (i * 3))/3} </text>])
+            <text key={'minorXText' + i} x='1' y={(i * 3) + .4} style={textStyle}> {(30 - (i * 3))/3} </text>])
       }
     }
     return minorXAxis
@@ -76,14 +76,14 @@ export default class MinorAxis extends Component {
         
       if (i % 2 === 0){
         minorYAxis.push([<line key={'minorY' + i} x1={i * 5 + 5} y1={svgHeight - 8} x2={i * 5 + 5} y2='0' style={{stroke:'grey',strokeWidth:'.1'}}/>,
-        <text x={i * 5 + 3.1} y={svgHeight - 6} style={textStyle}>{hour < 12 ? '0' + hour : hour}:{minutes}</text>,
-        <text x={i * 5 + 3.5} y={svgHeight - 4} style={textStyle}>{day}</text>,
-        <text x={i * 5 + 3.1} y={svgHeight - 2} style={textStyle}>{month} {index}</text>])
+        <text key={'minorYText' + i} x={i * 5 + 3.1} y={svgHeight - 6} style={textStyle}>{hour < 12 ? '0' + hour : hour}:{minutes}</text>,
+        <text key={'minorYText' + i + 1} x={i * 5 + 3.5} y={svgHeight - 4} style={textStyle}>{day}</text>,
+        <text key={'minorYText' + i + 2} x={i * 5 + 3.1} y={svgHeight - 2} style={textStyle}>{month} {index}</text>])
       } else {
         minorYAxis.push([<line key={'minorY' + i} x1={i * 5 + 5} y1={svgHeight - 8} x2={i * 5 + 5} y2='0' style={{stroke:'grey',strokeDasharray:'1,.5',strokeWidth:'.1'}}/>,
-        <text x={i * 5 + 3.2} y={svgHeight - 6} style={textStyle}>{hour2 < 12 ? '0' + hour2 : hour2}:{minutes}</text>,
-        <text x={i * 5 + 3.5} y={svgHeight - 4} style={textStyle}>{day}</text>,
-        <text x={i * 5 + 3.3} y={svgHeight - 2} style={textStyle}>{month} {index}</text>])
+        <text key={'minorYText' + i} x={i * 5 + 3.2} y={svgHeight - 6} style={textStyle}>{hour2 < 12 ? '0' + hour2 : hour2}:{minutes}</text>,
+        <text key={'minorYText' + i + 1} x={i * 5 + 3.5} y={svgHeight - 4} style={textStyle}>{day}</text>,
+        <text key={'minorYText' + i + 2} x={i * 5 + 3.3} y={svgHeight - 2} style={textStyle}>{month} {index}</text>])
       }
     }
     return minorYAxis
@@ -91,7 +91,7 @@ export default class MinorAxis extends Component {
 
   render () {
   	return (
-      <g>
+      <g key = 'minor axis'>
       {this.makeMinorYAxis()}
       {this.makeMinorXAxis()}
       </g>
